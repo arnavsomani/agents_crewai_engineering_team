3afa76c6f1418fd188c0eac1c7f66019100eac04
arnavsomani <arnav.somani@nyu.edu>
2026-03-22 19:44:43 +0530

    crew_ai_update

--- src/crew.py
+++ src/crew.py
@@ -4,7 +4,7 @@ from crewai.project import CrewBase, agent, crew, task
 
 
 @CrewBase
-class EngineeringTeam():
+class EngineeringTeamCrew():
     """EngineeringTeam crew"""
 
     agents_config = 'config/agents.yaml'
